fc7b48f7ab71ec55bf979ae3641472647063d2ea
Khramov Alex <protiyalex@gmail.com>
2020-09-15 23:52:06 +0300

    create modules and mongoDB

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const analyticsRoutes = require("./routes/analytics");
+const categoryRoutes = require("./routes/category");
+const orderRoutes = require("./routes/order");
+const mongoose = require("mongoose");
+const positionRoutes = require("./routes/position");
+const keys = require("./config/keys");
 const app = express();
+
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => console.log("mongo работает"))
+  .catch((error) => console.log(error));
+
+app.use(require("morgan")("dev"));
+app.use(require("cors")());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+app.use("./routes/analytics", analyticsRoutes);
+app.use("./routes/category", categoryRoutes);
+app.use("./routes/order", orderRoutes);
+app.use("./routes/position", positionRoutes);
+
 app.get("/", (req, res) => {
   res.status(200).json({
     message: "mes",
---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://protiyAlex:qiNrfOUElOxehLYV@cluster0.pinuw.mongodb.net/crmDB?retryWrites=true&w=majority",
+};
